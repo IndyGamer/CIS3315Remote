0aaa75b83967e77006b3b10157e9cd35f7074d06
Joshua Allen <allenjn05@mansfield.edu>
2017-01-30 14:11:37 +0000

    Wrote guessNumber file.

--- src/elementary/guessNumber.java
+++ src/elementary/guessNumber.java
@@ -1,11 +1,30 @@
 package elementary;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Administrator
  */
 public class guessNumber {
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int answer = rand.nextInt(99)+1;
+        System.out.println("Joshua's Guessing Game");
+        System.out.println("Guess a number between 1 and 100");
+        while (true) {
+            System.out.println("Enter a number:");
+            int guess = sc.nextInt();
+            if (guess == answer) {
+                break;
+            } else if (guess >= answer) {
+                System.out.println("The nummber is lower. Try again.");
+            } else {
+                System.out.println("The number is higher. Try again.");
+            }
+        }
+        System.out.println("You Win!!! YAY!");
     }
 }
